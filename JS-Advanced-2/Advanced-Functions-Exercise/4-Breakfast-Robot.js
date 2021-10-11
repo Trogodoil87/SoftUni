f1fd12fe3dc89e81cd35f62bf6473f45c29a284f
Encho Janev <encho871337@gmail.com>
2021-10-11 22:09:17 +0300

    Adding Homeworks

--- JS-Advanced-2/Advanced-Functions-Exercise/4-Breakfast-Robot.js
+++ JS-Advanced-2/Advanced-Functions-Exercise/4-Breakfast-Robot.js
@@ -1,44 +1,107 @@
 function solution() {
-    let storage = {};
+    function error(ingridient) {
+        return `Error: not enough ${ingridient} in stock`
+    }
+
+    let storage = {
+        protein: 0,
+        carbohydrate: 0,
+        fat: 0,
+        flavour: 0
+    };
 
     let recipes = {
         apple: (quantity) => {
             let carbohydrateNeed = 1 * quantity;
             let flavourNeed = 2 * quantity;
-            if (storage.hasOwnProperty('carbohydrate') && storage.hasOwnProperty('flavour')) {
-                return storage['carbohydrate'] >= carbohydrateNeed ? storage['flavour'] >= flavourNeed ? 'Success' : 'Error: not enough flavour in stock' : 'Error: not enough carbohydrate in stock'
-            }
+
+            return storage['carbohydrate'] >= carbohydrateNeed ?
+                storage['flavour'] >= flavourNeed ?
+                    ['Success', 'flavour', flavourNeed, 'carbohydrate', carbohydrateNeed] :
+                    [error('flavour')] :
+                [error('carbohydrate')]
+
 
         },
         lemonade: (quantity) => {
+            let carbohydrateNeed = 10 * quantity;
+            let flavourNeed = 20 * quantity;
+
+            return storage['carbohydrate'] >= carbohydrateNeed ?
+                storage['flavour'] >= flavourNeed ?
+                    ['Success', 'carbohydrate', carbohydrateNeed, 'flavour', flavourNeed] :
+                    [error('flavour')] :
+                [error('carbohydrate')]
+
 
         },
         burger: (quantity) => {
+            let carbohydrateNeed = 5 * quantity;
+            let fatNeed = 7 * quantity;
+            let flavourNeed = 3 * quantity;
+
+            return storage['carbohydrate'] >= carbohydrateNeed ?
+                storage['flavour'] >= flavourNeed ?
+                    storage['fat'] >= fatNeed ?
+                        ['Success', 'carbohydrate', carbohydrateNeed, 'flavour', flavourNeed, 'fat', fatNeed] :
+                        [error('fat')] :
+                    [error('flavour')] :
+                [error('carbohydrate')];
 
         },
         eggs: (quantity) => {
+            let proteinNeed = 5 * quantity;
+            let fatNeed = 1 * quantity;
+            let flavourNeed = 1 * quantity;
+
+            return storage['protein'] >= proteinNeed ?
+                storage['fat'] >= fatNeed ?
+                    storage['flavour'] >= flavourNeed ?
+                        ['Success', 'protein', proteinNeed, 'fat', fatNeed, 'flavour', flavourNeed] :
+                        [error('protein')] :
+                    [error('fat')] :
+                [error('flavour')]
 
         },
         turkey: (quantity) => {
+            let proteinNeed = 10 * quantity;
+            let carbohydrateNeed = 10 * quantity;
+            let fatNeed = 10 * quantity;
+            let flavourNeed = 10 * quantity;
+
+            return storage['protein'] >= proteinNeed ?
+                storage['carbohydrate'] >= carbohydrateNeed ?
+                    storage['fat'] >= fatNeed ?
+                        storage['flavour'] >= flavourNeed ?
+                            ['Success', 'protein', proteinNeed, 'carbohydrate', carbohydrateNeed, 'fat', fatNeed, 'flavour', flavourNeed] :
+                            [error('protein')] :
+                        [error('carbohydrate')] :
+                    [error('fat')] :
+                [error('flavour')]
 
         }
     };
 
     let commands = {
         restock: (microElement, quantity) => {
-            if (!storage.hasOwnProperty(microElement)) {
-                storage[microElement] = 0;
-            }
-
             storage[microElement] += quantity;
             return 'Success';
         },
         prepare: (name, quantity) => {
             let recipe = recipes[name];
-            return recipe(quantity);
+            let result = recipe(quantity);
+
+            if (result[0] === 'Success') {
+                for (let i = 1; i < result.length - 1; i++) {
+                    if (i % 2 === 1) {
+                        storage[result[i]] -= result[i + 1];
+                    }
+                }
+            }
+            return result[0].toString()
         },
         report: () => {
-
+            return Object.entries(storage).map((x) => x[0] + '=' + x[1]).join(' ');
         }
     };
 
@@ -52,13 +115,13 @@ function solution() {
 }
 let manager = solution();
 
-console.log(manager("restock flavour 0")); // Success  
+console.log(manager("restock flavour 50")); // Success  
 
 console.log(manager("prepare lemonade 4")); // Error: not enough carbohydrate in stock  
 
-console.log(manager('restock carbohydrate 50'));
+console.log(manager('restock carbohydrate 10'));
 
-console.log(manager('restock flavour 0'));
+console.log(manager('restock flavour 10'));
 
 console.log(manager('prepare apple 1'));
 
--- JS-Advanced-2/Advanced-Functions-Lab/5-Command-Processor.js
+++ JS-Advanced-2/Advanced-Functions-Lab/5-Command-Processor.js
@@ -1,40 +1,39 @@
 function solution() {
     let str = '';
-
-    function append(text) {
-        str += text;
-        return str;
-    }
-
-    function removeStart(count) {
-        str = str.split('').filter((x, i) => i > count - 1).join('');
-    }
-
-    function removeEnd(count) {
-        str = str.split('').reverse().filter((x, i) => i > count - 1).reverse().join('');
-    }
-
-    function print() {
-        console.log(str);
-    }
-
-
-    return {
+    let result = {
         append,
         removeStart,
         removeEnd,
         print
     };
+
+    return result;
+
+    function append(text) {
+        str += text;
+        return result;
+
+    }
+
+    function removeStart(count) {
+        str = str.split('').filter((x, i) => i > count - 1).join('');
+        return result;
+
+    }
+
+    function removeEnd(count) {
+        str = str.split('').reverse().filter((x, i) => i > count - 1).reverse().join('');
+        return result;
+
+    }
+
+    function print() {
+        console.log(str);
+        return result;
+    }
+
 }
 
 let firstZeroTest = solution();
 
-firstZeroTest.append('123');
-
-firstZeroTest.append('45');
-
-firstZeroTest.removeStart(2);
-
-firstZeroTest.removeEnd(1);
-
-firstZeroTest.print();
+firstZeroTest.append('123').append('45').removeStart(2).removeEnd(1).print();
