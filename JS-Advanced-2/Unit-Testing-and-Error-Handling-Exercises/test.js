19cb7d339b35cbba28fd951a5c3eb0f6fe876ab5
Encho Janev <encho871337@gmail.com>
2021-10-14 16:32:09 +0300

    Adding Homewroks

--- JS-Advanced-2/Unit-Testing-and-Error-Handling-Exercises/test.js
+++ JS-Advanced-2/Unit-Testing-and-Error-Handling-Exercises/test.js
@@ -1,41 +1,79 @@
 let { expect } = require('chai');
-let { lookupChar } = require('./3-Char-Lookup');
+let mathEnforcer = require('./4-Math-Enforcer');
 
-describe('Retrive char at given index', () => {
-    it('returns undefined for first parameter not a string', () => {
-        expect(lookupChar(1, 1)).to.be.undefined;
+describe('Math operations', () => {
+    it('returns undefined for invlaid parameter', () => {
+        expect(mathEnforcer.addFive('')).to.be.undefined;
     });
 
-    it('returns undefined for second parameter not a number', () => {
-        expect(lookupChar('avb', 'a')).to.be.undefined;
+    it('returns added positive integer + 5', () => {
+        expect(mathEnforcer.addFive(1)).to.equal(6);
     });
 
-    it('returns undefined for first parameter not a string and second parameter not a number', () => {
-        expect(lookupChar(1, 'a')).to.be.undefined;
+    it('returns added negative integer + 5', () => {
+        expect(mathEnforcer.addFive(-1)).to.equal(4);
     });
 
-    it('returns undefined for empty string', () => {
-        expect(lookupChar('', 'a')).to.be.undefined;
+    it('returns added positive float + 5', () => {
+        expect(mathEnforcer.addFive(2.5)).to.equal(7.5);
     });
 
-    it('Correct Index', () => {
-        expect(lookupChar('abc', 0)).to.equal('a');
-        expect(lookupChar('abcd', 3)).to.equal('d');
+    it('returns added negative float + 5', () => {
+        expect(mathEnforcer.addFive(-4.5)).to.equal(0.5);
     });
 
-    it('Incorrect Index', () => {
-        expect(lookupChar('abc', -1)).to.equal('Incorrect index');
-    })
-
-    it('Incorrect Index', () => {
-        expect(lookupChar('abc', 4)).to.equal('Incorrect index');
+    it('returns undefined for invlaid parameter', () => {
+        expect(mathEnforcer.subtractTen('')).to.be.undefined;
     });
 
-    it('returns undefined for empty first and second parameter str', () => {
-        expect(lookupChar('','')).to.be.undefined;
-    }) 
-
-    it('Incorrect Index',() => {
-        expect(lookupChar('abv', 2.2)).to.be.undefined;
+    it('returns subtracted positive integer - 10', () => {
+        expect(mathEnforcer.subtractTen(100)).to.equal(90);
     });
+
+    it('returns subtracted negative integer - 10', () => {
+        expect(mathEnforcer.subtractTen(-11)).to.equal(-21);
+    });
+
+    it('returns subtracted positive float - 10', () => {
+        expect(mathEnforcer.subtractTen(100.5)).to.equal(90.5);
+    });
+
+    it('returns subtracted negative float - 10', () => {
+        expect(mathEnforcer.subtractTen(-100.5)).to.equal(-110.5);
+    });
+
+
+    it('returns undefined for invlaid parameter', () => {
+        expect(mathEnforcer.sum('', 1)).to.be.undefined;
+        expect(mathEnforcer.sum(1, '')).to.be.undefined;
+        expect(mathEnforcer.sum('', '')).to.be.undefined;
+    });
+
+    it('returns sum of two positive integers', () => {
+        expect(mathEnforcer.sum(1, 1)).to.equal(2);
+    });
+
+    it('returns sum of two negative integers', () => {
+        expect(mathEnforcer.sum(-1, -3)).to.equal(-4);
+    });
+
+    it('returns sum of one positive integer and one negative integer', () => {
+        expect(mathEnforcer.sum(-1, 2)).to.equal(1);
+        expect(mathEnforcer.sum(2, -1)).to.equal(1);
+    });
+
+    it('returns sum of two positive float numbers', () => {
+        expect(mathEnforcer.sum(1.5, 1.5)).to.equal(3);
+    });
+
+    it('returns sum of two negative float numbers', () => {
+        expect(mathEnforcer.sum(-1.5, -1.5)).to.equal(-3);
+    });
+
+    it('returns sum of one positive float and one negative float', () => {
+        expect(mathEnforcer.sum(-1.5, 1.6)).to.be.within(0.1, 0.2);
+        expect(mathEnforcer.sum(1.6, -1.5)).to.be.within(0.1, 0.2);
+
+    });
+
 });
