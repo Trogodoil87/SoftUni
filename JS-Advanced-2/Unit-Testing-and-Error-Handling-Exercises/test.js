b2235b9e41705f8f520b1953e55a3a310b2f83e5
Encho Janev <encho871337@gmail.com>
2021-10-14 15:03:34 +0300

    Adding Homeworks

--- JS-Advanced-2/Unit-Testing-and-Error-Handling-Exercises/test.js
+++ JS-Advanced-2/Unit-Testing-and-Error-Handling-Exercises/test.js
@@ -1,16 +1,36 @@
 let { expect } = require('chai');
-let { isOddOrEven } = require('./2-Even-Or-Odd');
+let { lookupChar } = require('./3-Char-Lookup');
 
-describe('Tests length of string', () => {
-    it('returns even', () => {
-        expect(isOddOrEven('abv')).to.equal('odd');
+describe('Retrive char at given index', () => {
+    it('returns undefined for first parameter not a string', () => {
+        expect(lookupChar(1, 1)).to.be.undefined;
     });
 
-    it('returns even', () => {
-        expect(isOddOrEven('ab')).to.equal('even');
+    it('returns undefined for second parameter not a number', () => {
+        expect(lookupChar('avb', 'a')).to.be.undefined;
     });
 
-    it('returns undefined', () => {
-        expect(isOddOrEven(1)).to.equal(undefined);
+    it('returns undefined for first parameter not a string and second parameter not a number', () => {
+        expect(lookupChar(1, 'a')).to.be.undefined;
+    });
+
+    it('returns undefined for empty string', () => {
+        expect(lookupChar('', 'a')).to.be.undefined;
+    });
+
+    it('Correct Index', () => {
+        expect(lookupChar('abc', 0)).to.equal('a');
+    });
+
+    it('Incorrect Index', () => {
+        expect(lookupChar('abc', -1)).to.equal('Incorrect index');
     })
+
+    it('Incorrect Index', () => {
+        expect(lookupChar('abc', 4)).to.equal('Incorrect index');
+    });
+
+    it('returns undefined for empty first and second parameter str', () => {
+        expect(lookupChar('','')).to.be.undefined;
+    }) 
 });
